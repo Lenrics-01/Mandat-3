0ae7d6bbf963b2976b493b2406840208740c4c78
Tahar Tiba <e6304876@cmaisonneuve.qc.ca>
2025-10-29 17:42:54 -0400

    rajoute de verfication dans le js et ajout csv

--- script.js
+++ script.js
@@ -7,12 +7,36 @@ document.addEventListener("click", () => {
     clickSound.play();
 });
 
+const data = [[nom, prenom, txt]];
+
+
+
+
 let nom = document.getElementById("name");
 let btn = document.getElementById("btn");
+let txt = document.getElementById("msg");
+let prenom = document.getElementById("prename");
+
+function verfication() {
+    if (nom.value === "" || prenom.value === "" || txt.value === "") {
+        alert("Veuillez remplir tous les champs du formulaire.");
+        return false;
+    }
+
+    return true;
+}
+
+btn.addEventListener("click", function(event) {
+
+        event.preventDefault();
+        if (verfication()) {
+            alert("Merci " + nom.value + ", votre message a bien été envoyé !");
+        }
 
 
-btn.addEventListener("click", function() {
-        location.reload();
-        nom.focus();
         nom.value = "";
+        prenom.value = "";
+        txt.value = "";
+        nom.focus();
+
 });
